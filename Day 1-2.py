b58394d0b4d7445ad8a528c1bb9d8b860c58e58a
Diego Hernandez <101081290+diegodevF@users.noreply.github.com>
2026-06-09 02:53:59 +0000

    Day 1-2 of Python

---
+++ Day 1-2.py
@@ -0,0 +1,22 @@
+#30 Dias de Python
+
+first_name = "Diego"
+last_name = "Hernandez"
+
+print("La diferencia es: ", len(first_name) - len(last_name))
+
+
+num_one, num_two = 4, 5
+total_var = num_one + num_two
+diff = num_one - num_two
+multi = num_one * num_two
+div = num_one % num_two
+expo = num_one ** num_two
+floor_division = num_one // num_two
+
+print("Suma :",total_var)
+print("Resta :",diff)
+print("Multiplicacion :",multi)
+print("Division :",div)
+print("Exponencia :",expo)
+print("Residuo :",floor_division)
